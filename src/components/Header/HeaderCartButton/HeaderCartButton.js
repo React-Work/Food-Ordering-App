c720b07161bf82366c21d67f358d4be848763bfe
Parminder Singh <parminder94171@gmail.com>
2022-11-05 13:05:33 +0530

    FEAT : Cart button animation on each cart updations

--- src/components/Header/HeaderCartButton/HeaderCartButton.js
+++ src/components/Header/HeaderCartButton/HeaderCartButton.js
@@ -1,10 +1,25 @@
+import { useState, useEffect, useContext } from "react";
+import CartContext from "../../../context/cart-context";
 import Button from "../../UI/Button/Button";
+import buttonStyles from "../../UI/Button/Button.module.css";
 import CartIcon from "../../UI/CartIcon/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const cartContext = useContext(CartContext);
+    const [isButtonHighlighted, setIsButtonHighlighted] = useState(false);
+    
+    useEffect(() => {
+        setIsButtonHighlighted(true);
+        const timer = setTimeout(()=>{
+            setIsButtonHighlighted(false);
+        }, 300)
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartContext.cart_item_count]);
     return (
-        <Button className={styles.button} bump onClick={props.onClick}>
+        <Button className={`${styles.button} ${ isButtonHighlighted ?  buttonStyles.bump : ""}` } onClick={props.onClick}>
             <div className={styles.icon}>
                 <CartIcon />
             </div>
